1cab7d16134202abee0f39c793988622297f4161
Gokul Rajan <gokulrajanoff@gmail.com>
2023-02-08 20:49:10 +0530

    added https

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 const express = require('express')
 const app = express()
-const port = 443
+const https = require('https')
+const http = require('http')
 app.use(express.static('public'))
 
 app.get('/test', (req, res) => {
   res.send('Hello World!')
 })
-
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+const devport = 8080
+app.listen(devport, () => {
+  console.log(`Example app listening on port ${devport}`)
 })
+
+http.createServer(app).listen(80)
+https.createServer({}, app).listen(443)
